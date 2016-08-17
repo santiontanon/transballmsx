bf34cf0c2abb3d55d46cc10e1a3e2154de875aa3
artrag <artrag@users.noreply.github.com>
2016-08-17 23:36:34 +0200

    new sat

--- vram_map.asm
+++ vram_map.asm
@@ -4,5 +4,5 @@
 ;		   defb 0x9F ; Reg# 3 [COLOR BASE ADDRESS]					 = 2000h ; hybrid mode for colors
 ;		   defb 0x00 ; Reg# 4 00000[PATTERN GENERATOR BASE ADDRESS]	 = 0000h ; hybrid mode for patterns
 
-;		   defb 0x36 ; Reg# 5 0[SPRITE ATTRIBUTE TABLE BASE ADDRESS] = 1b00h
+;		   defb 0x37 ; Reg# 5 0[SPRITE ATTRIBUTE TABLE BASE ADDRESS] = 1b80h
 ;		   defb 0x07 ; Reg# 6 00000[SPRITE PTRN GNRTR BASE ADDRESS]	 = 3800h
