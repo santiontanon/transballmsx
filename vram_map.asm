67239390c84fad66df608df331b4826ec26e8221
santiontanon <santi.ontanon@gmail.com>
2016-08-18 08:56:03 +0200

    removed some asm files that I veno idea what where they doing there

--- vram_map.asm
+++
@@ -1,8 +0,0 @@
-
-;		   defb 0x06 ; Reg# 2 0000[NAME TABLE BASE ADDRESS]			 = 1800h
-
-;		   defb 0x9F ; Reg# 3 [COLOR BASE ADDRESS]					 = 2000h ; hybrid mode for colors
-;		   defb 0x00 ; Reg# 4 00000[PATTERN GENERATOR BASE ADDRESS]	 = 0000h ; hybrid mode for patterns
-
-;		   defb 0x37 ; Reg# 5 0[SPRITE ATTRIBUTE TABLE BASE ADDRESS] = 1b80h
-;		   defb 0x07 ; Reg# 6 00000[SPRITE PTRN GNRTR BASE ADDRESS]	 = 3800h
